7fa2ad37a986d46da922e2507d96d237d83d9db3
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-14 20:15:04 +0200

    scripts moved to tests folder

---
+++ tests/norma_compila.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# This tiny detail strips from the argument the file extension,  keeping in name only the file name.
+# I reuse the filename to find the test files for each function i want to test the file name 
+name=${1%.c}
+norminette -R CheckForbiddenSourceHeader ../$1
+cc  -Wall -Wextra -Werror -o test_$name test_$name.c ../$1 ../ft_isalpha.c ../ft_isdigit.c
+./test_$name
