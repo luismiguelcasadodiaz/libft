b200bc9a847536caa30f413597b3cb6ac6c99971
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-14 20:22:27 +0200

    file movemente between folders

--- norma_compila.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This tiny detail strips from the argument the file extension,  keeping in name only the file name.
-# I reuse the filename to find the test files for each function i want to test the file name 
-name=${1%.c}
-norminette -R CheckForbiddenSourceHeader $1
-cc -Wall -Wextra -Werror -o test_$name test_$name.c $1
-./test_$name
