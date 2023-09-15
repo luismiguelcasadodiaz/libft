9d24d53a35f85d85372c5a48026f4bdf5d365911
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-15 15:18:37 +0200

    tests adjusted to norminette

--- tests/test_ft_isascii.c
+++ tests/test_ft_isascii.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_isascii.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 14:48:18 by luicasad          #+#    #+#             */
+/*   Updated: 2023/09/15 14:48:48 by luicasad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <ctype.h>
 #include <stdio.h>
 
@@ -8,9 +19,9 @@ int	main(void)
 	int	c;
 
 	c = 0;
-	while ( c <= 127)
+	while (c <= 127)
 	{
-		printf(" %d, %d\n", c, (isascii(c) != 0) && (ft_isascii(c) != 0) );
+		printf(" %d, %d\n", c, (isascii(c) != 0) && (ft_isascii(c) != 0));
 		c++;
 	}
 	return (0);
--- tests/test_ft_isdigit.c
+++ tests/test_ft_isdigit.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_isdigit.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 14:48:59 by luicasad          #+#    #+#             */
+/*   Updated: 2023/09/15 14:49:21 by luicasad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include <ctype.h>
 #include <stdio.h>
 
@@ -8,9 +19,9 @@ int	main(void)
 	int	c;
 
 	c = 48;
-	while ( c <= 58)
+	while (c <= 58)
 	{
-		printf(" %d, %d\n", c, (isdigit(c) != 0) && (ft_isdigit(c) != 0) );
+		printf(" %d, %d\n", c, (isdigit(c) != 0) && (ft_isdigit(c) != 0));
 		c++;
 	}
 	return (0);
