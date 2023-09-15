f656bd883af91b4da093d1eceb7e4af4dea5ae0f
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2023-09-15 09:16:55 +0200

    Update ft_isalnum.c
    Comment the function

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -13,6 +13,9 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 
+/* The isalnum() function tests for any character for which isalpha(3) or     */
+/* isdigit(3) is true. The value of the argument must be representable as an  */
+/* unsigned char or the value of EOF.                                         */
 int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
