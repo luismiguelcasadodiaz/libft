16b6d67db5cd92213ae85705d41fd6c11de4cbb9
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2023-09-15 09:37:23 +0200

    Insert comments from man in the function

--- ft_isprint.c
+++ ft_isprint.c
@@ -12,6 +12,22 @@
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 
+/* ft_is_punc is a helper funciton to verificate the differente character     */
+/* ranges.
+/*   040 sp        041 ``!''     042 ``"''     043 ``#''     044 ``$''        */
+/*   045 ``%''     046 ``&''     047 ``'''     050 ``(''     051 ``)''        */
+/*   052 ``*''     053 ``+''     054 ``,''     055 ``-''     056 ``.''        */
+/*   057 ``/''                                                                */
+/*                                                                            */
+/*   072 ``:''     073 ``;''     074 ``<''     075 ``=''     076 ``>''        */
+/*   077 ``?''     100 ``@''                                                  */
+/*                                                                            */
+/*   133 ``[''     134 ``\''     135 ``]''     136 ``^''     137 ``_''        */
+/*   140 ```''                                                                */
+/*                                                                            */
+/*   173 ``{''     174 ``|''     175 ``}''     176 ``~''                      */
+/*                                                                            */
+
 int	ft_is_punc(int c)
 {
 	int	is_punc;
@@ -23,6 +39,9 @@ int	ft_is_punc(int c)
 	return (is_punc);
 }
 
+/* The isprint() function tests for any printing character, including space   */
+/* (` ').  The value of the argument must be representable as an unsigned     */
+/* char or the value of EOF.                                                  */
 int	ft_isprint(int c)
 {
 	return (ft_isdigit(c) || ft_isalpha(c) || ft_is_punc(c));
