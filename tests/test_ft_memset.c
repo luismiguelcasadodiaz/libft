dc388789f28d7429e817b114150764538c9ec998
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-19 14:26:59 +0200

    Create one buffer for me one for system function

--- tests/test_ft_memset.c
+++ tests/test_ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 18:11:26 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/18 20:28:14 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/19 12:17:44 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
@@ -16,43 +16,52 @@
 
 void	*ft_memset(void *b, int c, size_t len);
 
-void	test(void *b, int c, size_t len)
+void	test(char *text, int c, size_t len)
 {
-	printf("I  >%s<,", (char *)ft_memset(b, c, len));
-	printf("he >%s<\n", (char *)memset(b, c, len));
+	int		num;
+	char	*mytext;
+	char	*histext;
+	
+	printf("set %zu bytes:", len);
+	num = 0;
+	while (text[num] != '\0')
+		num++;
+	mytext = (char *)malloc(num * sizeof(char));
+	mytext = strcpy(mytext, text);
+	printf("I >%s<,", (char *)ft_memset(mytext, c, len));
+	free(mytext);
+	histext = (char *)malloc(num * sizeof(char));
+	histext = strcpy(histext, text);
+	printf("he >%s<\n", (char *)memset(histext, c, len));
+	free(histext);
 }
 
-void	test_num(void *b, int c, size_t len)
+void	test_num(int c, size_t len)
 {
-	printf("I  >%d<,",*(int *)ft_memset(b, c, len));
-	printf("he >%d<\n",*(int *)memset(b, c, len));
+	int		mynum;
+	int		hisnum;
+	
+	mynum = 2147111111;
+	hisnum = 2147111111;
+	printf("set %zu bytes:", len);
+	printf("I  >%d<,",*(int *)ft_memset(&mynum, c, len));
+	printf("he >%d<\n",*(int *)memset(&hisnum, c, len));
 }
 
 int	main(void)
 {
-	char	text;
-	int		num;
-	char	*text2;
 
-	text = (char *)malloc(5 * sizeof(char));
-	text = strcpy(text, "Luis\0");
-	test(text, 65, 0);
-	test(text, 65, 1);
-	test(text, 65, 2);
-	test(text, 65, 3);
-	test(text, 65, 4);
-	test(text, 65, 5);
-	test(text, 65, 8);
-	text2 = (char *)malloc(1 * sizeof(char));
-	text2 = strcpy(text2, "");
-	test(text2, 97, 1);
-	free(text);
-	free(text2);
-	num = 2147111111;
-	test_num(&num, 0, 0);
-	test_num(&num, 0, 1);
-	test_num(&num, 0, 2);
-	test_num(&num, 0, 3);
-	test_num(&num, 0, 4);
+	test("Luis", 65, 0);
+	test("Luis", 65, 1);
+	test("Luis", 65, 2);
+	test("Luis", 65, 3);
+	test("Luis", 65, 4);
+	test("Luis", 65, 5);
+	test("Luis", 65, 8);
+	test_num(0, 0);
+	test_num(0, 1);
+	test_num(0, 2);
+	test_num(0, 3);
+	test_num(0, 4);
 	return (0);
 }
