6615864f4def870066dcf36d03cdf0baa76fcd30
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-22 20:29:56 +0200

    first try but no possible in Ubuntu

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,6 +6,31 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:54 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/15 15:12:29 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/22 19:09:14 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+void	force_sigsegv(void)
+{
+	int	*p;
+
+	p = NULL;
+	*p = 1;
+}
+
+/* ft_strlcpy copy the input string into a destination string.                */
+/* If the destination buffer, limited by its size, isn't large enough to hold */
+/* the copy, the resulting  string  is  truncated  (but  it  is  guaranteed   */
+/* to be null-terminated).                                                    */
+/*                                                                            */
+/* They return the length of the total string they tried to create.           */
+/*                                                                            */
+/* These functions force a SIGSEGV if the src pointer is not a string.        */
+/*                                                                            */
+size_t	*ft_strlcpy(char *dst, const char *src, size_t sz)
+{
+	dst[0] = src[0];
+	sz = 2 * sz;
+	return (0);
+}
