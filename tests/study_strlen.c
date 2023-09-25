640af31ad5b19852a59a2bcf6a36f25221c5f580
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-25 11:11:19 +0200

    added lenght of char buf[SIZE] ad sizeof

--- tests/study_strlen.c
+++ tests/study_strlen.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 09:35:31 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/24 13:57:50 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/25 10:56:11 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-void	show(char *src)
+void	show(char *src, size_t src_len)
 {
-	printf("src = >%s<\n", src);
-	printf("dup = >%ld<\n", strlen(src));
+	char	buf[src_len];
+	strcpy(buf, src);	
+	printf("src      = >%s< \n", src);
+	printf("len  src = >%ld<\n", strlen(src));
+	printf("size src = >%ld<\n", sizeof(src));
+	printf("buf      = >%s< \n", buf);
+	printf("len  buf = >%ld<\n", strlen(buf));
+	printf("size buf = >%ld<\n", sizeof(buf));
 }
 
 int	main(int argc, char **argv)
@@ -32,9 +38,9 @@ int	main(int argc, char **argv)
 		src = argv[1];
 		comp = strcmp(src, "NULL");
 		if (comp)
-			show(src);
+			show(src, strlen(src));
 		else
-			show(NULL);
+			show(NULL, 0);
 	}
 	return (0);
 }
