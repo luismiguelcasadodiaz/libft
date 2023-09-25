c4d521615aec3220420b7f9cd789290380c9a802
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-25 19:24:09 +0200

    ft_substr Implementation, test, norma

--- ft_substr.c
+++ ft_substr.c
@@ -6,6 +6,39 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/15 15:15:38 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:14:39 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+/*                                                                            */
+/* ft_substr() returns a substring from the string ’s’. The substring begins  */
+/* at index ’start’ and is of maximum size ’len’.                             */
+/*                                                                            */
+/*                                                                            */
+/* GETS                                                                       */
+/*  s: The string from which to create the substring.                         */
+/*  start: The start index of the substring in the string ’s’.                */
+/*  len: The maximum length of the substring.                                 */
+/*                                                                            */
+/* RETURNS                                                                    */
+/* The substring or NULL if the allocation fails.                             */
+/*                                                                            */
+/* OPERATION                                                                  */
+/*  calculate substring size wiht starting point and num chars to extract.    */
+/*  When start + num chars to extract is bigger than string size, subs'size   */
+/*  must be string size minus starting point                                  */
+/*                                                                            */
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*s_sub;
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	if (start + len > s_len)
+		len = s_len - start;
+	s_sub = (char *)malloc(len + 1);
+	if (s_sub != NULL)
+		s_len = ft_strlcpy(s_sub, (s + start), len);
+	return (s_sub);
+}
---
+++ tests/test_ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_substr.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 09:35:31 by luicasad          #+#    #+#             */
+/*   Updated: 2023/09/25 19:20:14 by luicasad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../libft.h"
+
+int	main(int argc, char **argv)
+{
+	char	*str;
+	size_t	start;
+	size_t	len;
+
+	if (argc != 4)
+		printf("Usage ./test_ft_substr str start len");
+	else
+	{
+		len = (size_t)ft_atoi(argv[3]);
+		str = argv[1];
+		start = ft_atoi(argv[2]);
+		printf("str = >%s<, ", str);
+		printf("substr >%s<\n", ft_substr(str, start, len));
+		printf("str = >%s<, ", str);
+		printf("substr >%s<\n", ft_substr(str, start, 0));
+	}
+	return (0);
+}
+/*
+		printf("big = >%s<, ", (char *)((void *)0));
+		printf("mine >%s<, ", ft_strnstr((char *)((void *)0), *num));
+		printf("yours  >%s<\n", strnstr((char *)((void *)0), *num));
+		*/
