945407e51213b6f651c3d49804549b4b7618e6c6
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-26 19:45:29 +0200

    changes to pass paco

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:54 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/25 18:21:13 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/26 19:33:53 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,6 +14,16 @@
 /* The ft_strnstr() function locates the first occurrence of the null-term    */
 /* string little in the string big, where not more than len characters are	  */
 /* searched. Characters that appear after a `\0' character are not  searched. */
+/*                                                                            */
+/* GETS                                                                       */
+/*  big: a null-terminated string where to find the 'lit'  substr.            */
+/*  lit: a null-terminated string to find inside 'big'.                       */
+/*  len: the max num of chars of 'big' to start the search of 'lit'           */
+/*                                                                            */
+/* RETURNS                                                                    */
+/*  'big' when 'lit'  equals ""'                                              */
+/*  NULL  when 'lit' does not occur in 'big'.                                 */
+/*  a pointer to the first char of 'big' where starts the first 'lit'         */
 /*                                                                            */
 /* OPERATION                                                                  */
 /*  Loops big till either finds a match or max_idx                            */
@@ -25,14 +35,10 @@
 /* is smaller than little.                                                    */
 /* Also is useless to start a match verification after len chars verified     */
 
-size_t	max_idx(const char	*big, const char *lit, size_t len)
+static size_t	max_idx(size_t big_len, size_t lit_len, size_t len)
 {
-	size_t	big_len;
-	size_t	lit_len;
 	size_t	last_idx_big;
 
-	big_len = ft_strlen(big);
-	lit_len = ft_strlen(lit);
 	last_idx_big = 0;
 	if (lit_len < big_len)
 		last_idx_big = big_len - lit_len;
@@ -42,29 +48,49 @@ size_t	max_idx(const char	*big, const char *lit, size_t len)
 		return (len);
 }
 
-char	*ft_strnstr(const char	*big, const char *little, size_t len)
+static char	*my_find(const char	*b, const char *l, size_t ll, size_t len)
 {
 	size_t	i_big;
 	size_t	i_lit;
-	short	found;
 	size_t	i_big_max;
+	short	found;
 
-	i_big_max = max_idx(big, little, len);
+	i_big_max = max_idx(ft_strlen(b), ll, len);
 	i_big = 0;
 	found = 0;
 	while (!found && (i_big <= i_big_max))
 	{
 		found = 1;
 		i_lit = 0;
-		while (found && (little[i_lit] != '\0'))
+		while (found && (l[i_lit] != '\0'))
 		{
-			found = (big[i_big + i_lit] == little[i_lit]);
+			found = (b[i_big + i_lit] == l[i_lit]);
 			i_lit++;
 		}
 		i_big++;
 	}
 	if (found)
-		return ((char *)(big + --i_big));
+		return ((char *)(b + (--i_big)));
 	else
+		return ((void *)0);
+}
+
+char	*ft_strnstr(const char	*big, const char *lit, size_t len)
+{
+	char	*result;
+	size_t	lit_len;
+
+	if (len == 0)
 		return (NULL);
+	if ((big[0] != '\0') && (len > 0)) 
+	{
+		lit_len = ft_strlen(lit);
+		if (len >= lit_len)
+			result = my_find(big, lit, lit_len, len);
+		else
+			result = NULL;
+	}
+	else
+		result = (char *)(big);
+	return (result);
 }
--- tests/test_ft_strnstr.c
+++ tests/test_ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 09:35:31 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/25 18:39:41 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/26 19:43:19 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,10 @@ int	main(int argc, char **argv)
 		lit = argv[2];
 		printf("big = >%s<, ", big);
 		printf("mine >%s<\n", ft_strnstr(big, lit, len));
-//		printf("yours  >%s<\n", strnstr(big, lit, len));
+  		printf("yours  >%s<\n", strnstr(big, lit, len));
 		printf("big = >%s<, ", big);
 		printf("mine >%s<\n", ft_strnstr(big, lit, '\0'));
-//		printf("yours  >%s<\n", strnstr(big, lit, '\0'));
+  		printf("yours  >%s<\n", strnstr(big, lit, '\0'));
 	}
 	return (0);
 }
@@ -41,4 +41,32 @@ int	main(int argc, char **argv)
 		printf("big = >%s<, ", (char *)((void *)0));
 		printf("mine >%s<, ", ft_strnstr((char *)((void *)0), *num));
 		printf("yours  >%s<\n", strnstr((char *)((void *)0), *num));
-		*/
+		
+Error in test 3: ft_strnstr(0x101f9cda0: "", "", 0): expected: 0x101f9cda0, yours: 0x0
+Error in test 7: ft_strnstr(0x101f9cda0: "", "teste", 1): expected: 0x0, yours: 0x101f9cda0
+Error in test 8: ft_strnstr(0x101f9cda0: "", "teste", 2): expected: 0x0, yours: 0x101f9cda0
+Error in test 9: ft_strnstr(0x101f9b820: "teste", "", 0): expected: 0x101f9b820, yours: 0x0
+Error in test 20: ft_strnstr(0x101f9b8e0: "aaxx", "xx", 2): expected: 0x0, yours:
+0x101f9b8e2
+Error in test 21: ft_strnstr(0x101f9b8e0: "aaxx", "xx", 3): expected: 0x0, yours:
+0x101f9b8e2
+Error in test 25: ft_strnstr(0x7ffeedc6e680: "", "xx", 4294967295): expected: 0x0, yours:
+0x7ffeedc6e680
+
+
+KO  26: 	 check(ft_strnstr(empty, "", 0) == empty); showLeaks();
+KO  27: 	 check(ft_strnstr(empty, "coucou", -1) == 0); showLeaks();
+KO  29: 	 check(ft_strnstr(empty, "12345", 5) == 0); showLeaks();
+KO  31: 	 check(ft_strnstr(haystack, "cd", 8) == NULL); showLeaks();
+KO  33: 	 check(ft_strnstr("1", "a", 1) == NULL); showLeaks();
+
+Test 9:
+    if (!(str = ft_strnstr("lorem ipsum dolor sit amet", "dolor", 15)))
+        ft_print_result("NULL");
+    else
+        ft_print_result(str);
+Expected (cat -e test09.output):
+NULL
+Your result (cat -e user_output_test09):
+dolor sit amet
+*/
