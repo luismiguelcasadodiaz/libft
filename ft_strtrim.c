6cb58c6e74700005c4f6c5944c374c0cefc59600
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-09-29 18:49:19 +0200

    tryinf to remove last memory problem

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/27 20:27:56 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/29 18:47:32 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -43,22 +43,22 @@ char	*ft_strtrim(char const *s, char const *set)
 	size_t	s_ini;
 	size_t	s_end;
 	char	*t;
-	size_t	t_idx;
 
+	if (!s || !set)
+		return (NULL);
+	if (s[0] == '\0' && set[0] == '\0')
+	{
+		t= (char *)malloc(sizeof(char));
+		t[0] = '\0';
+		return (t);
+	}
 	s_len = ft_strlen(s);
 	s_ini = 0;
 	while ((s_ini <= s_len) && (ft_strchr(set, s[s_ini]) != NULL))
 		s_ini++;
-	s_end = 0;
+	s_end = s_len - 1;
 	while ((0 <= s_end) && (ft_strchr(set, s[s_end]) != NULL))
 		s_end--;
-	t = (char *)malloc((s_end - s_ini + 1) + 1);
-	if (t != NULL)
-	{
-		t_idx = 0;
-		while (s_ini <= s_end)
-			t[t_idx++] = s[s_ini++];
-		t[t_idx] = '\0';
-	}
+	t = ft_substr(s, s_ini, (s_end - s_ini + 1));
 	return (t);
 }
--- tests/test_ft_strtrim.c
+++ tests/test_ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/20 09:35:31 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/23 19:23:11 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/09/29 11:49:39 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@
 int	main(int argc, char **argv)
 {
 	char			*src;
+	char			*set;
 
-	if (argc != 2)
-		printf("Usage ./test_ft_strtrim str");
+	if (argc != 3)
+		printf("Usage ./test_ft_strtrim str set");
 	else
 	{
 		src = argv[1];
+		set = argv[2];
 		printf("src = >%s<, ", src);
-		printf("int >%s<\n", ft_strtrim(src));
+		printf("set = >%s<, ", set);
+		printf("tri >%s<\n", ft_strtrim(src, set));
 		printf("src = >\\0<, ");
-		printf("int >%s<,\n", ft_strtrim((char *) '\0'));
+		printf("tri >%s<,\n", ft_strtrim((char *)'\0', set));
 		printf("src = >(null)<, ");
-		printf("int >%s<,\n", ft_strtrim((char *)((void *)0)));
+		printf("tri >%s<,\n", ft_strtrim((char *)((void *)0), set));
 	}
 	return (0);
 }
