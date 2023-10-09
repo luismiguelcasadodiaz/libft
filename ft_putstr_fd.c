ac348cce58379ecf9835b695bbcf28247eeb282f
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-09 18:09:05 +0200

    four ft_put* functions paco, norm ok

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/10/09 16:58:13 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/10/09 18:05:43 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 
-/* ft_putchar_fd() outputs the character 'c' to the given file descriptor fd. */
+/* ft_putstr_fd() outputs the string 's' to the given file descriptor fd. */
 /*                                                                            */
 /* GETS                                                                       */
-/*  char c: the character to output.                                          */
+/*  char c: the string to output.                                          */
 /*  int fd: the file descriptor on which to write.                            */
 /*                                                                            */
 /* RETURNS                                                                    */
 /*  None .                                                                    */
 
-void	ft_putchar_fd(char c, int fd)
+void	ft_putstr_fd(char *s, int fd)
 {
-	write(fd, &c, 1);
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		write(fd, &s[i++], 1);
 }
