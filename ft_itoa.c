bd262bf0e2d0d4a4c2b7618cf85ad4922fe00986
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-11 01:30:14 +0200

    I complicated my life with an approach to allocate memory for the string  representing the integer gradually as new digits are required. You can see  it in this repository in ft_itoa_one_by_one.c
     As i did not manage to pass the fsoares test ft_itoa(0), then i figured
     out this other approach i can call ft_itoa_all_at_once.c
    
     I learn to deal with a pointer passed by reference to a function

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,94 +6,77 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/10/10 09:16:01 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/10/11 01:28:14 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 /* ************************************************************************** */
-/* create_empty_string()  allocates space for an empty string                 */
+/* allocate_space_for() allocates memory to show int n as a string            */
 /*                                                                            */
 /* GETS                                                                       */
-/*  *str_size : pointer to the size of the string to append c.                */
+/*  n   : is the integer to convert to string                                 */
+/*  *pos: is string length for required to show int n as a string             */
 /*                                                                            */
 /* RETURNS                                                                    */
-/*  NULL when there is not availabel memory                                   */
-/*  A pointer to a "" string                                                  */
+/*  Nothing, but worth to know that pos is a variable passed by reference     */
+/*  That communicates to calling function the string's length of the required */
+/*  string to show int n as an string.                                        */
 /*                                                                            */
-static char	*create_empty_string(size_t	*str_size)
+/* OPERATES                                                                   */
+/*  When n is negative (n <  0) at least requires 2-figure string.            */
+/*  When n is positive (0 <= n) at least requires 1-figure string.            */
+/*                                                                            */
+/*  n /= 10 removes less significative digit from n.                          */
+/*                                                                            */
+/*  While there are digits it counts and removes digits.                      */
+/*                                                                            */
+/*  Once number of digits in known, allocates memory for the figures plus the */
+/*  string null-terminator.                                                   */
+/*                                                                            */
+/*  if there is memory availabel initializes the string with white spaces and */
+/*  returns the new creates string.                                           */
+/*                                                                            */
+char	*allocate_space_for(int n, size_t *pos)
 {
 	char	*str;
+	size_t	figures;
 
-	*str_size = 0;
-	str = (char *)malloc(1 * sizeof(char));
+	if (n < 0)
+		figures = 2;
+	else
+		figures = 1;
+	n /= 10;
+	while (n)
+	{
+		figures = figures + 1;
+		n /= 10;
+	}
+	str = (char *)malloc((figures + 1) * sizeof(char));
 	if (str != NULL)
 	{
-		str[0] = '\0';
-		*str_size = 1;
+		str[figures] = '\0';
+		*pos = figures - 1;
+		while (figures)
+			str[--figures] = ' ';
+		str[figures] = ' ';
 	}
 	return (str);
 }
 
 /* ************************************************************************** */
-/* add_digit()  appends a char in the tail of an string                       */
-/*                                                                            */
-/*                                                                            */
-/* GETS                                                                       */
-/*  c : the char to append to str                                             */
-/*  **str :Address of the pointer the string to append to char at tail        */
-/*  *str_size : pointer to the size of the string to append c.                */
-/*                                                                            */
-/* RETURNS                                                                    */
-/*  Nothing, but it worths to be aware of the fact that **str and *str_size   */
-/*  change. they return new string and new string size.                       */
-/*                                                                            */
-/*                                                                            */
-/*  OPERATION                                                                 */
-/*  Allocates memory for one string a char longer than str_size.              */
-/*                                                                            */
-/*  Copies str_size bytes of memory from str, to new_str.                     */
-/*                                                                            */
-/*  Insert c in the tail of new_str.                                          */
-/*                                                                            */
-/*  null-terminate new_str.                                                   */
-/*                                                                            */
-/*  Update the *str_size to new_str length                                    */
-/*                                                                            */
-/*  free the memory of str                                                    */
-/*                                                                            */
-/*  Change the memroy address of the pointer referencing to str               */
-/*                                                                            */
-static void	add_digit(char c, char **str, size_t *str_size)
-{
-	char	*new_str;
-
-	new_str = (char *)malloc((*str_size + 1) * sizeof(char));
-	if (new_str)
-	{
-		ft_memcpy(new_str, *str, *str_size);
-		new_str[*str_size -1] = c;
-		new_str[*str_size] = '\0';
-		*str_size = *str_size + 1;
-		free(*str);
-		*str = new_str;
-	}
-	else
-		*str = new_str;
-}
-
-/* ************************************************************************** */
 /* aux_itoa()is a recursive function that extract digits from an integer and  */
-/* concatenates them at the tail of the string str                            */
+/* inserts them in the right position of a string with the right spaces to    */
+/* hold all figures comming from the integer.                                 */
 /*                                                                            */
 /* GETS                                                                       */
-/*  n : the integer to extract digits from                                    */
-/*  **str :Address of the pointer to  the string to append to digit           */
-/*  *str_size : pointer to the size of the string to append digit.            */
+/*  n     : The integer to extract digits from.                               */
+/*  **str : Address of the pointer to  the string to append to digit          */
+/*  *pos  : The lenght of the string that holds the integer.                  */
 /*                                                                            */
 /* RETURNS                                                                    */
-/*  Nothing, but worths to be aware that thru **str and *str_size there is    */
-/*  communication with the calling function.                                  */
+/*  Nothing, but worths to be aware that thru **str there is communication    */
+/*  with the calling function.                                                */
 /*                                                                            */
 /* OPERATES                                                                   */
 /*  Treates diferently accordingly to integer's sign.                         */
@@ -112,25 +95,31 @@ static void	add_digit(char c, char **str, size_t *str_size)
 /*             ----------------------------------------                       */
 /*                         num = 48 + (-n % 10)                               */
 /*  That does not work with INT_MIN                                           */
-static void	aux_itoa(int n, char **str, size_t *str_size)
+/*                                                                            */
+/*  Recursively the function extract the less significant figure from the int */
+/*  in such a way the figure is inserted in the string from the right to the  */
+/*  left, from n towards zero index. Then in each recursive call we have to   */
+/*  decrease the position where to insert the figure (pos -1)                 */
+/*                                                                            */
+
+static void	aux_itoa(int n, char **str, size_t pos)
 {
 	char	num;
 
 	if (0 < n)
 	{
 		if (9 < n)
-			aux_itoa(n / 10, str, str_size);
+			aux_itoa(n / 10, str, pos - 1);
 		num = 48 + n % 10;
-		add_digit(num, &(*str), str_size);
 	}
 	else
 	{
 		if (n < -9)
-			aux_itoa(n / 10, str, str_size);
+			aux_itoa(n / 10, str, pos - 1);
 		num = n % 10;
 		num = 48 - num;
-		add_digit(num, &(*str), str_size);
 	}
+	str[0][pos] = num;
 }
 
 /* ft_itoa() allocates (with malloc(3)) and returns a strings representing    */
@@ -145,38 +134,45 @@ static void	aux_itoa(int n, char **str, size_t *str_size)
 /*   NULL if memory allocation fails.                                         */
 /*                                                                            */
 /* OPERATION                                                                  */
-/*  Starts creating an empty string.                                          */
+/*  Starts creating an empty (filled wiht spaces) string of enought lenght    */
+/*  to hold all figures in the integer.                                       */
 /*                                                                            */
-/*  When n = 0 add a '0' to the empty string                                  */
+/*  When n = 0 add a '0' to the empty string n the position 0 of a string of  */
+/*  length 1.                                 */
+/*                                                                            */
 /*  When n is positive convert the integer into string                        */
-/*  When n is negative add '-' to the empty string and convert the integer    */
+/*                                                                            */
+/*  When n is negative convert the integer into string and add '-' to head    */
 /*                                                                            */
 /* PROBLEMS I GOT                                                             */
-/* I complicated my life with this approach. It aims to allocate memory for   */
-/* the string representing the integer gradually as new digits are required.  */
+/* I complicated my life with an approach to allocate memory for the string   */
+/* representing the integer gradually as new digits are required. You can see */
+/* it in this repository in ft_itoa_one_by_one.c                              */
+/*                                                                            */
+/* As i did not manage to pass the fsoares test ft_itoa(0), then i figured    */
+/* out this other approach i can call ft_itoa_all_at_once.c                   */
 /*                                                                            */
 /* I learn to deal with a pointer passed by reference to a function           */
 
 char	*ft_itoa(int n)
 {
 	char	*str;
-	size_t	str_size;
+	size_t	pos;
 
-	if (n == 0)
+	str = allocate_space_for(n, &pos);
+	if (str)
 	{
-		str = create_empty_string(&str_size);
-		add_digit('0', &str, &str_size);
-	}
-	else if (n > 0)
-	{
-		str = create_empty_string(&str_size);
-		aux_itoa(n, &str, &str_size);
-	}
-	else
-	{
-		str = create_empty_string(&str_size);
-		add_digit('-', &str, &str_size);
-		aux_itoa(n, &str, &str_size);
+		if (n == 0)
+			str[0] = '0';
+		if (n > 0)
+		{
+			aux_itoa(n, &str, pos);
+		}
+		if (n < 0)
+		{
+			aux_itoa(n, &str, pos);
+			str[0] = '-';
+		}
 	}
 	return (str);
 }
