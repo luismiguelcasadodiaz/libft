70cb7e68aade7d407c8c448317d9d654372a421c
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-12 16:17:50 +0200

    Ft_lstclear implemented, documented, paco OK

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,6 +6,40 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/15 11:00:56 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:16:36 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+/* ft_lstlast() returns the last node of the list.                            */
+/*                                                                            */
+/* GETS                                                                       */
+/*  *lst : The address of a pointer to a node.                                */
+/*  *del : The address of the function used to delete the content of the node */
+/*                                                                            */
+/* RETURNS                                                                    */
+/*  Nothing                                                                   */
+/*                                                                            */
+/* OPERATION                                                                  */
+/*                                                                            */
+/*   loops the list while there is a node to loop throu                       */
+/*                                                                            */
+/*   Inside the while 'actual' keeps the address of node.                     */
+/*   'lst' moves to next node of actual to check again the while condition.   */
+/*   At list end, lst will get NULL, as required in the specification.        */
+/*                                                                            */
+/*   calls del function to remove actual's content.                           */
+/*   release actual memory                                                    */
+/*                                                                            */
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*actual;
+
+	while (*lst)
+	{
+		actual = *lst;
+		*lst = actual->next;
+		del(actual->content);
+		free(actual);
+	}
+}
