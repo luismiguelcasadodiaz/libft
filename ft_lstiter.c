0a01edc2ce92de24284d82289cbb6854ac07735b
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-12 16:31:36 +0200

    ft_lstiter implemented, documented, paco OK

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,6 +6,37 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:55 by luicasad          #+#    #+#             */
-/*   Updated: 2023/09/15 14:42:05 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/10/12 16:31:03 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
+
+/* ft_lstiter() iterates the list and applies th funciotn 'f' to the content  */
+/* of each node                                                               */
+/*                                                                            */
+/* GETS                                                                       */
+/*  *lst : The address of a pointer to a node.                                */
+/*  *del : The address of the function used to iterate on teh list            */
+/*                                                                            */
+/* RETURNS                                                                    */
+/*  Nothing                                                                   */
+/*                                                                            */
+/* OPERATION                                                                  */
+/*                                                                            */
+/*   loops the list while there is a node to loop throu                       */
+/*                                                                            */
+/*   Inside the while 'actual' keeps the address of node.                     */
+/*   'lst' moves to next node of actual to check again the while condition.   */
+/*   At list end, lst will get NULL, as required in the specification.        */
+/*                                                                            */
+/*   calls del function to remove actual's content.                           */
+/*   release actual memory                                                    */
+/*                                                                            */
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
