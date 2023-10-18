25310a8f101a932e5cc7e881341a1fe2907068e7
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-10-18 19:12:56 +0200

    Remove proteccions, add errno

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,10 @@
 /*   By: luicasad <luicasad@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 18:02:54 by luicasad          #+#    #+#             */
-/*   Updated: 2023/10/17 18:35:55 by luicasad         ###   ########.fr       */
+/*   Updated: 2023/10/18 19:00:36 by luicasad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-#include <limits.h>
 
 /* The ft_calloc() function allocates memory for an array of nmemb elements   */
 /* of size bytes each and returns a pointer to the allocated memory.          */
@@ -43,17 +42,20 @@
 /* With this protection Francinette was ok , but no Moulinette.               */
 /*                                                                            */
 /*                                                                            */
+/*	if (nmemb == 0 && size == 0)                                              */
+/*		return ((void *)malloc(1));                                           */
+/*	if (size == 0)                                                            */
+/*		return ((void *)0);                                                   */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
 
-	if (nmemb == 0 && size == 0)
-		return ((void *)malloc(1));
-	if (size == 0)
-		return ((void *)0);
 	result = (void *)malloc(nmemb * size);
 	if (result == NULL)
+	{
+		errno = ENOMEM;
 		return ((void *)0);
+	}
 	else
 	{
 		ft_bzero(result, nmemb * size);
